e6607ba1069a4085cc5ebb9036e19fec04acbe4b
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2021-11-09 19:34:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ console.log("My socket server is running");
 var express = require('express');
 var app = express();
 var server = app.listen(3000);
-function listen(){
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Listening at http://" + host + ":" + port);
-}
+
 app.use(express.static('public'));
 
 var io = require("socket.io")(server);
-
+io.set('transports', ['polling']);
 io.sockets.on("connection", function(socket){
   console.log("New client: " + socket.id);
   socket.on('mouse', function(data){
@@ -20,4 +16,7 @@ io.sockets.on("connection", function(socket){
   socket.on('disconnect', function(){
     console.log("Client disconnected: " + socket.id);
   })
+ // setInterval(function(){
+  //  console.log(socket.conn.transport.name);
+ // },2000)
 })
